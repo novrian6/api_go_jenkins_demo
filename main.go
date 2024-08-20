27ce6a7885bd871780180b87b88959e67482e4ac
Nova Novriansyah <liebera6@gmail.com>
2024-08-20 14:12:18 +0700

    ..

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "Hello, World apa kabar nova")
-	fmt.Fprintf(w, "Baris ini ditulis oleh nova")
+	fmt.Fprintf(w, "Hello, World apa kabar nova...")
+	fmt.Fprintf(w, "Baris ini ditulis oleh nova...")
 
 }
 
